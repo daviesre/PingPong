05d2af41c270c214b2cbdb9de238ea53f1a96f97
Russ Davies <russdavies392@gmail.com>
2016-06-24 10:48:04 -0700

    Add basic submit feature and jquery to prepare for user input

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-for (counter = 1; counter < 51; counter++) {
-  if (counter % 15 === 0) {
-    console.log("ping-pong");
-  } else if (counter % 3 === 0) {
-    console.log("ping");
-  } else if (counter % 5 === 0) {
-    console.log("pong");
-  } else {
-    console.log(counter);
+var pingPong = function(ping) {
+  for (counter = 1; counter < 21; counter++) {
+    if (counter % 15 === 0) {
+      console.log("ping-pong");
+    } else if (counter % 3 === 0) {
+      console.log("ping");
+    } else if (counter % 5 === 0) {
+      console.log("pong");
+    } else {
+      console.log(counter);
+    }
   }
 }
+
+
+$(document).ready(function() {
+  $("form#tabletennis").submit(function(event){
+    event.preventDefault();
+    
+  });
+
+});
