955eed836ad42a3172ea269a669518073022a9d7
Russ Davies <russdavies392@gmail.com>
2016-06-24 10:29:39 -0700

    Replace 'ping-pong' with any number divisible by 15 in the loop

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-for (counter = 1; counter < 21; counter++) {
-  if (counter % 3 === 0) {
+for (counter = 1; counter < 51; counter++) {
+  if (counter % 15 === 0) {
+    console.log("ping-pong");
+  } else if (counter % 3 === 0) {
     console.log("ping");
-  } else if (counter % 5 ===0) {
+  } else if (counter % 5 === 0) {
     console.log("pong");
   } else {
     console.log(counter);
