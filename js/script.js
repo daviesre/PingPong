1f8b7190ae98c6b6f387cde1afdd7e4fed880461
Russ Davies <russdavies392@gmail.com>
2016-06-24 14:03:46 -0700

    Add option to submit and play again without having to refresh th epage

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
   $("form#userSubmit").submit(function(event) {
    var numInput = $("input#NumInput").val();
    event.preventDefault();
+   $("#list").html("");
       for (var counter = 1; counter <= numInput; counter++) {
         if (counter % 15 === 0) {
          $("#list").append("<li>"+"ping-pong"+"</li>");
-         console.log("pong");
         } else if (counter % 3 === 0) {
             $("#list").append("<li>"+"ping"+"</li>");
         } else if (counter % 5 === 0) {
