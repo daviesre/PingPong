cd90eee8a787eebf6de2047dd644630badf56982
Russ Davies <russdavies392@gmail.com>
2016-06-24 12:04:07 -0700

    Add more functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
-var pingPong = function(ping) {
-  for (counter = 1; counter < numInput; counter++) {
-    if (counter % 15 === 0) {
-      console.log("ping-pong");
-    } else if (counter % 3 === 0) {
-      console.log("ping");
-    } else if (counter % 5 === 0) {
-      console.log("pong");
-    } else {
-      console.log(counter);
-    }
-  }
-}
-
 $(document).ready(function() {
-  $("form#tabletennis").submit(function(event){
-    event.preventDefault();
-    var numInput = $("input#wordInput").val();
-    $("#result span").text(pingPong(numInput));
-    $("result").show(pingPong(numInput));
-    console.log()
-
-
+  $("form#userSubmit").submit(function(event) {
+   var num = $("input#wordInput").val();
+   event.preventDefault();
+      for (var counter = 1; counter <= num; counter++) {
+        if (counter % 15 === 0) {
+         $("#list").append("<li>"+"ping-pong"+"</li>");
+         console.log("pong");
+        } else if (counter % 3 === 0) {
+            $("#list").append("<li>"+"ping"+"</li>");
+        } else if (counter % 5 === 0) {
+            $("#list").append("<li>"+"pong"+"</li>");
+        } else {
+            $("#list").append("<li>"+ counter + "</li>");
+        }
+      }
   });
 });
