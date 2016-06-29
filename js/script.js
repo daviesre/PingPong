24491997b1405a5cd8b12a4aa13618a0b094d324
Russ Davies <russdavies392@gmail.com>
2016-06-28 17:49:55 -0700

    separate back end from front end logic

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
+function calculate(numInput) {
+  var input = [];
+  for (var i = 1; i <= numInput; i++) {
+    if (i % 15 === 0) {
+     input.push("ping-pong");
+   } else if (i % 3 === 0) {
+        input.push("ping");
+    } else if (i % 5 === 0) {
+        input.push("pong");
+    } else {
+        input.push(i);
+    }
+
+  }
+  return input;
+};
+
 
 $(document).ready(function() {
   $("form#userSubmit").submit(function(event) {
-   var numInput = $("input#NumInput").val();
+   var numInput = parseInt($("input#NumInput").val());
    event.preventDefault();
    $("#list").html("");
-      for (var counter = 1; counter <= numInput; counter++) {
-        if (counter % 15 === 0) {
-         $("#list").append("<li>"+"ping-pong"+"</li>");
-        } else if (counter % 3 === 0) {
-            $("#list").append("<li>"+"ping"+"</li>");
-        } else if (counter % 5 === 0) {
-            $("#list").append("<li>"+"pong"+"</li>");
-        } else {
-            $("#list").append("<li>"+ counter + "</li>");
-        }
-      }
+
+   var output = calculate(numInput);
+   $("#list").text(output);
+
+
   });
 });
